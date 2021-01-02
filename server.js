d46a06e4064f3c2b66f47cf17d48e5f54cca0346
Racer <reaganjoseph26@yahoo.com>
2021-01-02 18:39:28 -0500

    validation added with POST request

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express()
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true })); 
+//The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object. 
+//The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -47,6 +57,32 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
       let results = animals
       if(req.query) {
@@ -63,6 +99,20 @@ app.get('/api/animals/:id', (req, res) => {
          res.sendStatus(404);
        }
 });
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
 
 app.listen(PORT, () => {
       console.log(`API server now on port ${PORT}!`);
