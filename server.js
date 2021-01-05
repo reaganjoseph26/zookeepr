67fcff0c3873865bcdcbdb59051ad9836e90afed
Racer <reaganjoseph26@yahoo.com>
2021-01-05 17:39:37 -0500

    html routes moved

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
+// const { animals } = require('./data/animals');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -12,21 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 //The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
 // parse incoming JSON data
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
 
 app.listen(PORT, () => {
       console.log(`API server now on port ${PORT}!`);
